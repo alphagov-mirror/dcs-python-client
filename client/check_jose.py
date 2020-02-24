bff2688154f0bab89cc69cee2f700e07e431bf5b
Benjamin Gill <benjamin.gill@digital.cabinet-office.gov.uk>
2020-02-24 14:19:43 +0000

    Sg1ST7qV: check that thumbprints are correct
    Compare the provided against the expected and tell users if they differ.
    
    Co-authored-by: Kerr Rainey <kerr.rainey@digital.cabinet-office.gov.uk>

--- client/check_jose.py
+++ client/check_jose.py
@@ -22,12 +22,12 @@ import base64
 import requests
 import urllib.parse
 import uuid
+import sys
 
 
-
-def generate_thumbprints(path):
+def generate_thumbprints(cert_path):
     """Generate the thumbprints needed for the `x5t` and `x5t256` headers"""
-    with open(path, "rb") as f:
+    with open(cert_path, "rb") as f:
         cert = x509.load_pem_x509_certificate(f.read(), default_backend())
 
     sha1_thumbprint = (
@@ -48,6 +48,28 @@ def generate_thumbprints(path):
 
     return sha1_thumbprint, sha256_thumbprint
 
+
+def check_thumbprints(cert_path, user_sha1, user_sha256):
+    expected_sha1, expected_sha256 = generate_thumbprints(cert_path)
+
+    if (expected_sha1, expected_sha256) != (user_sha1, user_sha256):
+        print("Thumbprints don't match")
+        print(f"  SHA1 - Expected {expected_sha1}, was {user_sha1}")
+        print(f"  SHA256 - Expected {expected_sha256}, was {user_sha256}")
+        sys.exit(1)
+
+    print("Success - thumbprints are correct")
+
+
 def main():
     arguments = docopt(__doc__)
-    print(arguments)
+
+    check_thumbprints(
+        arguments["--client-signing-certificate"],
+        arguments["--sha1-thumbprint"],
+        arguments["--sha256-thumbprint"],
+    )
+
+
+if __name__ == "__main__":
+    main()
